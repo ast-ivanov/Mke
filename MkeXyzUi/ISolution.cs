357dc38c4f1ba654b6373fea5f584a23afb659a1
ast-ivanov <arch.step.inc@gmail.com>
2018-05-02 01:04:50 +0700

    Summary

--- MkeXyzUi/ISolution.cs
+++ MkeXyzUi/ISolution.cs
@@ -2,8 +2,11 @@
 {
     public interface ISolution
     {
+        /// <summary>Параметры решателя</summary>
         SolutionParams SolutionParams { get; set; }
 
+        /// <summary>Получить решение</summary>
+        /// <returns>Веса</returns>
         (double[] q, double[] U) Calculate();
     }
 }
